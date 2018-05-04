9dae0aa4ee89f8b08dfe0d2c8ad049373c522825
ostapmlnk <ostap.melnik.1996@mail.ru>
2018-05-04 10:59:40 +0300

    add file

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,7 +1,23 @@
-def translate
-  vowels = ['a','i','o','u','e']
-  words = sent.split(' ')
-  if
-    word.include.vowels
+def translate(a)
+  if a.split(' ').size > 1
+    a.map {|x| pig(x)}.join(' ')
+  else
+    pig(a)
+  end
+end
+
+def pig(word)
+  vowels = %w(a o i e u)
+  alphabet = ('a'..'z').to_a
+  consonant = alphabet - vowels
+
+  if vowels.include? word[0]
+    word + 'ay'
+  elsif consonant.include? word[0] && word[1]
+    word[2..-1] + word[0..1] + 'ay'
+  elsif consonant.include? word[0]
+    word[1..-1] + word[0] + 'ay'
+  else
+    word
   end
 end
